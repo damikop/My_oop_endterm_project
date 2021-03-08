03392255191a3c4f1027746761ce9a463a01d971
Damilya <68182683+damikop@users.noreply.github.com>
2021-03-08 23:08:26 +0600

    Update IDB.java

--- java_part/IDB.java
+++ java_part/IDB.java
@@ -3,6 +3,7 @@ package com.company;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+//interface for PostgresDB
 public interface IDB {
     Connection getConnection() throws SQLException, ClassNotFoundException;
 }
