7bcf38808ad9baa430386d9b455b921e2a16c9fb
Damilya <68182683+damikop@users.noreply.github.com>
2021-03-08 23:08:00 +0600

    Update IBookRepository.java

--- java_part/IBookRepository.java
+++ java_part/IBookRepository.java
@@ -1,6 +1,10 @@
 package com.company;
+
 import com.company.Book;
+
 import java.util.List;
+
+// Repository: to reduce the effort to implement data access layers for various persistence stores significantly.
 public interface IBookRepository {
     List<Book> getAllBooks();
     Book getBook(int ISBN);
