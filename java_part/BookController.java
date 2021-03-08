bbc0af9f4270862c9420036ef3ef7ae88fb7770b
Damilya <68182683+damikop@users.noreply.github.com>
2021-03-08 23:06:47 +0600

    Update BookController.java

--- java_part/BookController.java
+++ java_part/BookController.java
@@ -1,32 +1,37 @@
 package com.company;
+
 import com.company.Book;
 import com.company.IBookRepository;
 
 import java.util.List;
-public class BookController {
-    private final IBookRepository repo2;
 
-    public BookController(IBookRepository repo2) {
-        this.repo2 = repo2;
+//this class for creating book operations: view, find and add
+public class BookController {
+    //implementing the interface of repository
+    private final IBookRepository dinoRepo2;
+
+    public BookController(IBookRepository dinoRepo2) {
+        this.dinoRepo2 = dinoRepo2;
     }
 
+    //view all books from database
     public String getAllBooks() {
-        List<Book> books = repo2.getAllBooks();
+        List<Book> books = dinoRepo2.getAllBooks();
         return books.toString();
     }
 
+    //find book by its ISBN (ID)
     public String getBook(int ISBN) {
-        Book book = repo2.getBook(ISBN);
-
+        Book book = dinoRepo2.getBook(ISBN);
+        //if database does not contain given ID it will output "Book was not found!"
         return (book == null ? "Book was not found!" : book.toString());
     }
-
+    
+    //
     public String addBook(int ISBN, String title, String author, String category) {
 
         Book book = new Book(ISBN, title, author, category);
-
-        boolean created2 = repo2.addBook(book);
-
-        return (created2 ? "Book was added!" : "Book did not add!");
+        boolean dinoCreated2 = dinoRepo2.addBook(book);
+        return (dinoCreated2 ? "Book was added!" : "Book did not add!");
     }
 }
