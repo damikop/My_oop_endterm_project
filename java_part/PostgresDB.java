d142eef4eb265233b472977cd8ed622529243548
Damilya <68182683+damikop@users.noreply.github.com>
2021-03-08 23:09:56 +0600

    Update PostgresDB.java

--- java_part/PostgresDB.java
+++ java_part/PostgresDB.java
@@ -1,18 +1,28 @@
 package com.company;
+
 import com.company.IDB;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.*;
+
 public class PostgresDB implements IDB{
+    
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
-        Connection connection = null;
+        
+        Connection dinoConnection = null;
+       
         try {
+            //Here we load the driver’s class file into memory at the runtime
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:1780/lbrjava", "postgres", "Qqwerty1!");
-            return connection;
-        } catch (Exception e) {
-            System.out.println(e);
+            
+            //Establishing the connection with DB
+            dinoConnection = DriverManager.getConnection("jdbc:postgresql://localhost:1780/lbrjava", "postgres", "Qqwerty1!");
+            
+            return dinoConnection;        
+        } 
+        catch (Exception dinoE) {
+            System.out.println(dinoE);
             return null;
         }
     }
